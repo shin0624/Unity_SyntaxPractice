eaa6fe16c42af2e45b21d364509637bf8d1f8ba8
bbs <91828379+shin0624@users.noreply.github.com>
2024-01-24 22:02:24 +0900

    플레이어 상태 관리를 위한 PlayerState 추가
    PlayerState에서 모든 상태를 관리하도록 변경
    1) UpdateDie() / UpdateMoving() / UpdateIdle()
    2) UpdateMoving() : Raycast로 마우스 클릭 시 캐릭터 이동, 애니메이션 실행 코드 복사
    3) UpdateIdle() : 대기상태 애니메이션 실행 코드 복사

--- PlayerCotroller.cs
+++ PlayerCotroller.cs
@@ -1,23 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCotroller : MonoBehaviour
 {
     [SerializeField]
     float _speed = 10.0f;//접근지정자를 public으로 하거나 SerializeField를 사용하면 인스펙터에서 속도값을 수정 가능
-    float _yAngle = 0.0f;//로테이션 조작을 위한 변수 생성
-    
+                         // float _yAngle = 0.0f;//로테이션 조작을 위한 변수 생성
+
+    //bool _moveToDest = false;//_desPos 사용 여부(목적지로 이동하는지) --->PlayerState로 관리할 것이므로 주석처리
+    Vector3 _destPos;//목적지
+
+    float wait_run_ratio = 0;
+
+    public enum PlayerState //Player의 상태를 관리할 PlayerState 열거체
+    {
+        Die,
+        Moving,
+        Idle
+
+    }
+
+    PlayerState _state = PlayerState.Idle;
+
+    void UpdateDie()
+    {
+
+    }
+    void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;//Click한 목적지 좌표 - Player좌표 = 이동할 방향벡터(방향+거리)
+        if (dir.magnitude < 0.0001f)
+        {
+            //player가 목적지에 도달했으므로 idle상태로 변경한다.
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude); //-->연산하는 거리가, 현재 Player와 destPos 간 거리보다 적어야함을 보장하는 Clamp(value, min, max)-->value값 정도에 따라 min,max로 자동조절
+                                                                                     //transform.position+= dir.normalized* _speed * Time.deltaTime;//(거리 = 속도*시간)
+            transform.position += dir.normalized * moveDist;
+
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);//만약 Player회전이 느리다면 상수값을 20정도로 늘림
+
+            //transform.LookAt(_destPos);//이동할 때 destPos 방향으로 시선 고정
+        }
+        // RUN 애니메이션 처리
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);//Lerp함수를 이용하여 Player움직임을 부드럽게 표현-->변수 wait_run_ratio가 10.0f * deltatime의 간격으로 조금씩 1에 가까워져 RUN 상태로.
+        Animator anim = GetComponent<Animator>();//GetComponent로 Animator를 가져온다
+        anim.SetFloat("wait_run_ratio", wait_run_ratio);//애니메이터에서 wait_run_ratio라는 이름으로 블렌딩한 파라미터(float)를 세트시켜준다. 1에 가까울수록 RUN애니메이션 실행
+        anim.Play("WAIT_RUN");
+
+
+
+    }
+    void UpdateIdle()
+    {
+        //Idle (=wait) 애니메이션 처리
+        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);//Lerp함수를 이용하여 Player움직임을 부드럽게 표현-->변수 wait_run_ratio가 10.0f * deltatime의 간격으로 조금씩 0에 가까워져 WAIT상태로.
+        Animator anim = GetComponent<Animator>();//GetComponent로 Animator를 가져온다
+        anim.SetFloat("wait_run_ratio", wait_run_ratio);//애니메이터에서 wait_run_ratio라는 이름으로 블렌딩한 파라미터(float)를 세트시켜준다. 0에 가까울수록 WAIT애니메이션 실행--->Player의 움직임이 더 부드러워질 것.
+        anim.Play("WAIT_RUN");
+    }
+
+
     void Start()
     {
-        
+        //Managers.Input.KeyAction -= OnKeyboard;//중복 호출을 방지하기 위한 마이너스 --->PlayerState로 관리할 것이므로 주석처리
+        // Managers.Input.KeyAction += OnKeyboard;
+
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
     // 업데이트 메서드는 프레임 당 한번씩, 즉 1/60초마다 한번씩 실행되므로
     //실제 사용에 맞추기 위해 이전 프레임과 지금 프레임의 시간 차이(Time.deltaTime)를 이용한다
     //시간 * 속도 = 거리 를 이용
+
     void Update()
     {
+
+#if WASD_Move_1
         //전후좌우 이동 : new Vector3(0.0f, 0.0f, 1.0f) 사용 또는 예약어 사용(월드좌표계)  
         // 로컬 좌표계(Player의 시선을 기준으로 한 전후좌우 : transform.TransformDirection(로컬->월드)
         //(월드->로컬) : InverseTransformDirection
@@ -36,8 +101,7 @@ public class PlayerCotroller : MonoBehaviour
         //if (Input.GetKey(KeyCode.D))//우
         //     transform.Translate(Vector3.right * Time.deltaTime * _speed);
 
-        //2. rotation 조작
-        _yAngle += Time.deltaTime * 100.0f;
+        //2. rotation 조작  
         //1)절대 회전값 고정(transform.eulerAngles 사용)
         // transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);//오일러 각 사용
 
@@ -54,7 +118,94 @@ public class PlayerCotroller : MonoBehaviour
 
         //전후좌우 GetKey if문을 수정
 
+#endif
 
+#if moveToDest_will_managing_PlayerState
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;//Click한 목적지 좌표 - Player좌표 = 이동할 방향벡터(방향+거리)
+            if(dir.magnitude < 0.0001f)
+            {
+                _moveToDest = false;//Player가 destPos로 도착하면 다시 moveToDest를 false로 바꾼다.
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude); //-->연산하는 거리가, 현재 Player와 destPos 간 거리보다 적어야함을 보장하는 Clamp(value, min, max)-->value값 정도에 따라 min,max로 자동조절
+                //transform.position+= dir.normalized* _speed * Time.deltaTime;//(거리 = 속도*시간)
+                transform.position+= dir.normalized* moveDist;
+
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);//만약 Player회전이 느리다면 상수값을 20정도로 늘림
+
+                //transform.LookAt(_destPos);//이동할 때 destPos 방향으로 시선 고정
+            }
+        
+        
+        }
+#endif
+
+#if Animation_will_managing_PlayerState
+        //애니메이션 적용
+        if (_moveToDest)//Player가 dest까지 움직이고 있다면
+        {
+            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);//Lerp함수를 이용하여 Player움직임을 부드럽게 표현-->변수 wait_run_ratio가 10.0f * deltatime의 간격으로 조금씩 1에 가까워져 RUN 상태로.
+            Animator anim = GetComponent<Animator>();//GetComponent로 Animator를 가져온다
+            anim.SetFloat("wait_run_ratio", wait_run_ratio);//애니메이터에서 wait_run_ratio라는 이름으로 블렌딩한 파라미터(float)를 세트시켜준다. 1에 가까울수록 RUN애니메이션 실행
+            anim.Play("WAIT_RUN");
+        }
+        else
+        {
+            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);//Lerp함수를 이용하여 Player움직임을 부드럽게 표현-->변수 wait_run_ratio가 10.0f * deltatime의 간격으로 조금씩 0에 가까워져 WAIT상태로.
+            Animator anim = GetComponent<Animator>();//GetComponent로 Animator를 가져온다
+            anim.SetFloat("wait_run_ratio", wait_run_ratio);//애니메이터에서 wait_run_ratio라는 이름으로 블렌딩한 파라미터(float)를 세트시켜준다. 0에 가까울수록 WAIT애니메이션 실행--->Player의 움직임이 더 부드러워질 것.
+            anim.Play("WAIT_RUN");
+        }
+#endif
+
+        switch (_state) //PlayerState로 플레이어 상태를 관리하기 위해 switch 문을 추가
+        {
+            case PlayerState.Die:
+                UpdateDie(); break;
+
+            case PlayerState.Moving:
+                UpdateMoving(); break;
+
+            case PlayerState.Idle:
+                UpdateIdle(); break;
+
+        }
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt)//InputManager 스크립트에서 Action<Define.MouseEvent> MouseAction 으로 선언하였으므로, 마우스이벤트 객체를 인자로 넘겨줌
+    {
+        //---마우스 클릭, 클릭 유지 중에도 달리는 애니메이션이 재생될 수 있도록 주석처리
+        //if (evt != Define.MouseEvent.Click)// 들어온 이벤트가 Click이 아니면 무시
+        //    return;
+        //--TestCollision에서 이동한 Raycasting 부분 --->LOL처럼, 마우스로 바닥을 클릭하면 클릭한 곳을 destPos로 하여 그곳까지 Player를 움직이게 할 것임.
+
+        if (_state == PlayerState.Die)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))//인자에 LayerMask를 바로 추가
+        {
+
+            _destPos = hit.point;//hit.point = Ray가 Hit한 컬라이더의 월드좌표. 이를 목적지로 하여 Player를 이동시킬 것-->Update()에서 목적지로 이동시키면 된다.
+                                 //_moveToDest = true;--->PlayerState로 관리할 것이므로 주석처리
+
+            _state = PlayerState.Moving;
+        }
+
+
+    }
+
+#if OnKeyboard_will_managing_PlayerState
+    void OnKeyboard()
+    {
         if (Input.GetKey(KeyCode.W))//앞     
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.1f);
@@ -77,6 +228,10 @@ public class PlayerCotroller : MonoBehaviour
             transform.position += Vector3.right * Time.deltaTime * _speed;
         }
 
+        _moveToDest = false;//키보드로 조작할 때는 마우스 클릭으로 이동처럼 destPos를 정하고 움직이지 않으므로 false.
 
     }
+#endif
+
+
 }
